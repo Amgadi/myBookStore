b2a4ca75855f184ca5ba531268df3591777dd79b
Sahar Amgadi <saharamgadi@gmail.com>
2019-10-01 12:43:39 +0300

    Change bars

--- src/components/categoriesDropDown.js
+++ src/components/categoriesDropDown.js
@@ -3,15 +3,15 @@ import {Dropdown} from 'semantic-ui-react';
 import './style.css';
 
 const getCategoriesOptions = () => {
-    var obj = localStorage.getItem('Categories') ? localStorage.getItem('Categories') : JSON.stringify({categories : []});
-    obj = JSON.parse(obj);
-    obj.categories.map(category => {
+    var categoriesList = localStorage.getItem('Categories') ? localStorage.getItem('Categories') : JSON.stringify({categories : []});
+    categoriesList = JSON.parse(categoriesList);
+    categoriesList.categories.map(category => {
         category.key = category.name;
         category.text = category.name;
         category.value = category.name;
         return category
     });
-    return obj.categories;
+    return categoriesList.categories;
 }
 
 const CategoriesDropDown = (props) => {
